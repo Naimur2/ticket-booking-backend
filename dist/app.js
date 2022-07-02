28fc5dbaf8c8413c6da4ece93f38092d6396de22
naimur2 <naimurrahaman248@gmail.com>
2022-07-02 13:00:40 +0600

    first commit

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,11 +15,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = require("mongoose");
 const app = (0, express_1.default)();
 dotenv_1.default.config();
+const { env } = process;
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, cors_1.default)());
+app.use(express_1.default.static("public"));
+const run = () => __awaiter(void 0, void 0, void 0, function* () {
+    const DB_URL = env.MONGO_URI + env.MONGO_DB;
+    try {
+        yield (0, mongoose_1.connect)(DB_URL);
+        console.log("Connected to MongoDB");
+    }
+    catch (error) {
+        console.error(error);
+    }
+});
+run();
 const errorHandler = (err, req, res, next) => {
     if (err.headerSent) {
         return next(err);
@@ -19,7 +42,7 @@ const errorHandler = (err, req, res, next) => {
         message: err.message,
     });
 };
-const port = process.env.PORT || 8080;
+const port = env.PORT || 8080;
 app.listen(port, () => {
     console.log("Server is running on port 4000");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,31 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express, { ErrorRequestHandler, Express } from "express";
+import { connect } from "mongoose";
 
 const app: Express = express();
 dotenv.config();
+
+const { env } = process;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
+app.use(express.static("public"));
+
+const run = async (): Promise<void> => {
+    const DB_URL = (env.MONGO_URI as string) + env.MONGO_DB;
+
+    try {
+        await connect(DB_URL);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+run();
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
     if (err.headerSent) {
@@ -19,7 +36,7 @@ const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
     });
 };
 
-const port = process.env.PORT || 8080;
+const port: string | number = (env.PORT as string) || 8080;
 
 app.listen(port, () => {
     console.log("Server is running on port 4000");
